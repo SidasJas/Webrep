9efa6b2bdb2309a5c2cf724dac3a2f354d894f72
Arnaszd <163340386+Arnaszd@users.noreply.github.com>
2024-03-16 00:59:51 +0200

    Add files via upload

---
+++ Home Page/Contact Us Form/contactform.php
@@ -0,0 +1,21 @@
+<?php 
+
+if(isset($_POST['submit']))
+{
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+
+    $mailTo = "submit@phonkextended.info";
+    $headers = "From: ".$mailFrom;
+    $txt = "Gautas laiškas iš ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers)
+    {
+        header("Location: index.php?mailsend")
+    }
+    
+}   
+?>
---
+++ Home Page/index.php
@@ -0,0 +1,37 @@
+<?php
+include "head.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GASP</title>
+    <style>
+       
+        <?php include "stylesheet.css"; ?>
+    </style>
+</head>
+<body>
+<nav class="navbar">
+    <div class="logo">
+        <a href="#">ShellyAPI</a>
+    </div>
+    <ul>
+        <li><a href="index.php">Pagrindinis</a></li>
+        <li><a href="contact.php">Kontaktai</a></li>
+        <li><a href="about.php">Apie mus</a></li>
+        <li><a href="../Contact Us Form/index.php">Susisiekite su mumis</a></li>
+        <li><a href="signup.php">Prisijungimas</a></li>
+    </ul>
+
+</nav>
+    <div class="row">
+        <div class="column">
+            <h2>Shelly išmaniųjų namų sistema</h2>
+            <p>lorem ipsum lalalalalalla</p>
+        </div>
+    </div>
+</body>
+</html>
