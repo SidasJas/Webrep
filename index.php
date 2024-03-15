f3d27e3736bface30f82bb4a643db7e73beb890f
SidasJas <sidjas@ktu.lt>
2024-03-15 23:26:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 include "head.php";
+//sitas eilutes copy paste visuose pages
 include("connection.php");
 include("functions.php");
 
-//registracijos dalykai
+//registracijos dalykai. Tikrina ar LogedIn
     session_start();
     $user_data=check_login($con);
-
+//sitas eilutes copy paste visuose pages
 ?>
 
 <!DOCTYPE html>
