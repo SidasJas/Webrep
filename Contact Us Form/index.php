961f3a71a49f8a13f6539a88664e209dc8b52101
Arnaszd <163340386+Arnaszd@users.noreply.github.com>
2024-03-16 02:09:40 +0200

    Atsijungimo mygtuka idejau i contact form

--- Contact Us Form/index.php
+++ Contact Us Form/index.php
@@ -21,6 +21,7 @@
         <li><a href="../contact.php">Kontaktai</a></li>
         <li><a href="../about.php">Apie mus</a></li>
         <li><a href="index.php">Susisiekite su mumis</a></li>
+        <li><a href="logout.php">Atsijungti</a></li>
     </ul>
 
 </nav>
@@ -36,3 +37,4 @@
     </main>
 </body>
 </html>
+
